0cfaff931dbc6a6fa091bf83800e8684848967c1
Luz Fernández Acuña <luz.fernandezac@gmail.com>
2023-12-11 07:41:22 -0500

    agrega ui

---
+++ CODIGO/Assets/Script/QuitManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class QuitManager : MonoBehaviour
+{
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
+
+
